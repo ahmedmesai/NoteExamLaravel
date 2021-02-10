c7e17e9f1f9ac2940f6952e7d11c7efc966602a0
ahmedmesai <ahmedmessai777@gmail.com>
2021-02-10 19:23:51 +0100

    Finish Controller and Routes For Authentification API

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends BaseController
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+            'c_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validate Error', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('thisgottatobestrong')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User Created Successfully!');
+    }
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('thisgottatobestrong')->accessToken;
+            $success['name'] = $user->name;
+            return $this->sendResponse($success, 'User Logined Successfully!');
+        } else {
+            return $this->sendError('Please Verify Email Or Password');
+        }
+    }
+}
