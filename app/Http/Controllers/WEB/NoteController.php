44676c4ca28ca3d053c8a501b9b39ceed14686a5
ahmedmesai <ahmedmessai777@gmail.com>
2021-02-11 09:48:15 +0100

    Finish Contoller and Routes Note Web

---
+++ app/Http/Controllers/WEB/NoteController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\WEB;
+
+use App\Http\Controllers\Controller;
+use App\Models\Note;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NoteController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function index()
+    {
+        $notes = Auth::user()->notes()->latest()->get();
+        return view('notes.index')->with('notes', $notes);
+    }
+
+    public function create()
+    {
+        return view('notes.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $input = $request->all();
+        $input['user_id'] = Auth::id();
+        Note::create($input);
+
+        return redirect()->route('notes');
+    }
+
+
+    public function show($id)
+    {
+        $note = Note::find($id);
+        return view('notes.show', compact('note'));
+    }
+
+
+    public function edit($id)
+    {
+        $note = Note::find($id);
+        return view('notes.edit', compact('note'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $note = Note::find($id);
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $note->title = $request->title;
+        $note->content = $request->content;
+        $note->save();
+        return redirect()->route('notes');
+    }
+
+
+    public function destroy($id)
+    {
+        $note = Note::find($id);
+        $note->delete();
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +10,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::resource('notes', 'WEB\NoteController');
+Route::get('notes', 'WEB\NoteController@index')->name('notes');
+Route::get('notes/create/', 'WEB\NoteController@create')->name('note.create');
+Route::post('notes/store', 'WEB\NoteController@store')->name('note.store');
+Route::get('notes/show/{id}', 'WEB\NoteController@show')->name('note.show');
+Route::get('notes/edit/{id}', 'WEB\NoteController@edit')->name('note.edit');
+Route::post('notes/update/{id}', 'WEB\NoteController@update')->name('note.update');
+Route::get('notes/destroy/{id}', 'WEB\NoteController@destroy')->name('note.destroy');
